4357370e9c896fa7db1c71f6a3e9e342370b9e5f
A. Alfonso F. R. <alffore@gmail.com>
2014-08-08 14:17:58 -0500

    Se agrega el lector de Configuraciones XML
    Se recupera el modelo del XML de la aplicación Demo del Lector de la
    Antena

---
+++ Acuanet/Acuanet/LecConfigXML.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Acuanet
+{
+ 
+    //clase para leer configuraciones de un archivo XML
+    class LecConfigXML
+    {
+        XmlDataDocument doc;
+        XmlElement element;
+        XmlNode node;
+        string path;
+
+        #region Constructor
+        public LecConfigXML()
+        {
+            path = Application.StartupPath + "\\config.xml";
+            doc = new XmlDataDocument();
+
+            try
+            {
+                doc.Load(path);
+                element = doc.DocumentElement;
+            }
+            catch
+            {
+            }
+        }
+        #endregion
+
+
+        #region Properties
+        public string Filename
+        {
+            set
+            {
+                path = value;
+                doc.Load(path);
+                element = doc.DocumentElement;
+            }
+            get { return path; }
+        }
+        #endregion
+
+
+        public string Text(string key, string defaultValue)
+        {
+            node = doc.SelectSingleNode(key);
+            if (node == null)
+                return defaultValue;
+            else
+                return node.InnerText;
+        }
+
+        public int Int16(string key, int defaultValue)
+        {
+            int i;
+
+            node = doc.SelectSingleNode(key);
+            try
+            {
+                i = int.Parse(node.InnerText);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+            return i;
+        }
+
+        public bool Boolean(string key, bool defaultValue)
+        {
+            node = doc.SelectSingleNode(key);
+            if (node == null)
+            {
+                return defaultValue;
+            }
+
+            if (node.InnerText.Equals("true", StringComparison.OrdinalIgnoreCase) == true)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public bool Set(string key, string value)
+        {
+            node = doc.SelectSingleNode(key);
+            if (node == null) return false;
+            node.InnerText = value;
+            return true;
+        }
+
+        public void Save()
+        {
+            doc.Save(path);
+        }
+    }
+}
