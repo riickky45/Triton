5c3bd91920148a4a227f25955b8840e4ac8c5f6c
A. Alfonso F. R. <alffore@gmail.com>
2014-08-08 13:44:06 -0500

    Se agrega el nucleo principal de Lectura Eventos
    Se incluyen los codigos basicos de inserción  de Lecturas de Eventos
    (oleadas), simples marcas de tiempo asociadas con los tags, este codigo
    esta pensado para multihilos

---
+++ Acuanet/Acuanet/InsertaLecturaE.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+using CSL;
+
+namespace Acuanet
+{
+    //esta clase inserta las Lecturas (en formato Lista) del Evento correspondiente
+    class InsertaLecturaE
+    {
+        private List<Lectura> aLectura;
+
+        public InsertaLecturaE(List<Lectura> aLectura)
+        {
+            this.aLectura = aLectura;
+        }
+
+        //metodo que inserta el array de lectura en la BD
+        protected void insertaLecturaL()
+        {
+            string strConnect = "server=127.0.0.1;uid=root;pwd=XXXXXXX;database=YYYYYY";
+            MySqlConnection dbConn = new MySqlConnection(strConnect);
+            dbConn.Open();
+
+            foreach (Lectura d in aLectura)
+            {
+                string sql = "INSERT INTO TTTTTT (valor,id_tag) VALUES (" + d.t.ToString() + ",'" + d.t + "')";
+                MySqlCommand cmd = new MySqlCommand(sql, dbConn);
+                cmd.ExecuteNonQuery();
+            }
+            dbConn.Close();
+            dbConn = null;
+        }
+
+    }
+}
---
+++ Acuanet/Acuanet/Lectura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using CSL;
+
+namespace Acuanet
+{
+    //Clase para guardar una lectura de un tag en un evento, este posee toda la información necesaria
+    class Lectura
+    {
+        public TAG t;
+
+
+    }
+}
