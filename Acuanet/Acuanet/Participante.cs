5a94909c507d2b6d6969b57c7ff5f9fe0489865a
riickky45 <riickky45@icloud.com>
2014-08-08 22:22:24 -0500

    Update Participante.cs
    Se crea direcc y sclub

--- Acuanet/Acuanet/Participante.cs
+++ Acuanet/Acuanet/Participante.cs
@@ -9,6 +9,8 @@ namespace Acuanet
         public string nombre;
         public int id;
         public string snumero;
+        public string sclub;
         public string id_tag;
+        public string direcc;
     }
 }
