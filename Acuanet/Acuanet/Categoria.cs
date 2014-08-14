b24eb367f2878860abc04a1d6d3626c18dffe639
A. Alfonso F. R. <alffore@gmail.com>
2014-08-14 12:48:35 -0500

    Se agrega Categoria y el Modelo de Categoria

---
+++ Acuanet/Acuanet/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acuanet
+{
+    class Categoria
+    {
+
+        public string nombre;
+        public string desc;
+        public int id;
+
+    }
+}
---
+++ Acuanet/Acuanet/ModCategoria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Acuanet
+{
+    class ModCategoria
+    {
+        MySqlConnection dbConn;
+
+        public ModCategoria(string strConexion)
+        {
+            dbConn = new MySqlConnection(strConexion);
+            dbConn.Open();
+        }
+
+
+        public List<Categoria> obtenCategorias()
+        {
+            List<Categoria> lcat = new List<Categoria>();
+
+            return lcat;
+        }
+
+
+        public Categoria obtenCategoriaxId(int id)
+        {
+
+        }
+
+
+
+        ~ModCategoria()
+        {
+            dbConn.Close();
+            dbConn = null;
+        }
+
+    }
+}
