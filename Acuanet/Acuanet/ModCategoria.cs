f234caf8b272f0e19457924b0f991a23650c5f83
A. Alfonso F. R. <alffore@gmail.com>
2014-08-15 23:17:10 -0500

    Inclusion de Metodos para completar codigos
    completar la Clase ModCategoria.cs

--- Acuanet/Acuanet/ModCategoria.cs
+++ Acuanet/Acuanet/ModCategoria.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
@@ -16,7 +17,7 @@ namespace Acuanet
             dbConn.Open();
         }
 
-
+        //Obtiene el listado de las categorias en formato List
         public List<Categoria> obtenCategorias()
         {
             List<Categoria> lcat = new List<Categoria>();
@@ -40,7 +41,7 @@ namespace Acuanet
             return lcat;
         }
 
-
+        //Obtiene una sola Categoria en objeto recuperado por su ID
         public Categoria obtenCategoriaxId(int id)
         {
             Categoria cat = new Categoria();
@@ -61,12 +62,38 @@ namespace Acuanet
             return cat;
         }
 
-        public bool creaCategoria(Categoria c)
+        
+        public void creaCategoria(Categoria c)
         {
+            //codigo que se conecta y query de insercion
 
-            return true;
+            string sql = "INSERT INTO categoria (nombre,desc) VALUES ('" + c.nombre + "','" + c.desc + "')";            
+            MySqlCommand cmd = new MySqlCommand(sql, dbConn);
+            cmd.ExecuteNonQuery();
+
         }
 
+
+        public DataTable obtenDTC(){
+
+            string sql = "SELECT nombre,id,descripcion FROM categoria ORDER BY nombre ";
+            MySqlCommand cmd = new MySqlCommand(sql, dbConn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Categoria cat = new Categoria();
+                cat.nombre = rdr.GetString(0);
+                cat.id = System.Convert.ToInt32(rdr.GetString(1));
+                cat.desc = rdr.GetString(2);
+
+                
+
+            }
+            rdr.Close();
+
+            return null;
+        }
 
         ~ModCategoria()
         {
