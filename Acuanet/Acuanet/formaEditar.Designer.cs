faa4102b05a475c32aa42960ace249291bfe7f95
riickky45 <riickky45@icloud.com>
2014-08-20 10:32:31 -0500

    REcuperacion y algo de limpieza

--- Acuanet/Acuanet/formaEditar.Designer.cs
+++ Acuanet/Acuanet/formaEditar.Designer.cs
@@ -102,7 +102,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = global::Acuanet.Properties.Resources.app_search_32x32_32;
+            
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.Location = new System.Drawing.Point(25, 286);
             this.button1.Name = "button1";
@@ -117,7 +117,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Image = global::Acuanet.Properties.Resources.applications_32x32_321;
+
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.Location = new System.Drawing.Point(285, 286);
             this.button2.Name = "button2";
