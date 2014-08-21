d90568edea7658486cbf719152c3d63d313b689c
riickky45 <riickky45@icloud.com>
2014-08-20 19:14:47 -0500

    Cambio segun yo al boton

--- Acuanet/Acuanet/FormaPrincipal.cs
+++ Acuanet/Acuanet/FormaPrincipal.cs
@@ -86,7 +86,7 @@ namespace Acuanet
         private void btnOleada_Click(object sender, EventArgs e)
         {
             oleada.iniciaCaptura();
-            this.btnOleada.Text = "PARA";
+            this.btnOleada.Text = "PARALA";
         }
 
        
